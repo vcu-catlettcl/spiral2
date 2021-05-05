a00a7f3c3c447195ab5092eaadfa0edca116c311
vcu-catlettcl <78384776+vcu-catlettcl@users.noreply.github.com>
2021-05-04 23:36:34 -0400

    Attempt 1

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,16 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
+    n = input("Enter your value: ")
+    cnt = 0
+    inc = 2
+    return_value = 0
+
+    while n <= size ** 2:
+        return_value += n
+        n += inc
+        cnt += 1
+        if cnt == 4:
+            inc += 2
+            cnt = 0
+
     return_value = n
     return return_value
